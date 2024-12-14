5bdaa94af6d26d7b2e3c3017a474d5c29e7cb8c3
Sujan <work.sujan26@gmail.com>
2024-12-14 15:22:20 +0530

    setup expo router

---
+++ app/_layout.tsx
@@ -0,0 +1,7 @@
+import { Slot, Stack, Tabs } from 'expo-router';
+
+export default function RootLayout() {
+    return(
+        <Stack />
+    );
+}
---
+++ app/cart.tsx
@@ -0,0 +1,9 @@
+import {View, Text} from 'react-native';
+
+export default function Cart() {
+  return (
+    <View>
+      <Text style={{ fontSize: 30}}>Cart Screen</Text>
+    </View>
+  );
+}
